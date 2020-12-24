bc57b9bac1d4e901ab1c7c9fb0de5d22c8a43064
klofaes <vs201100@gmail.com>
2020-12-24 17:04:33 +0530

    Add mongoose connection to mongoDB

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express"); //*Find what this is used for
+const cors = require("cors"); //*Find what this is used for
+const mongoose = require("mongoose"); //*Find what this is used for
+
+require("dotenv").config(); //*Find what this is used for
+
+const app = express(); //*Find what this is used for
+
+app.use(cors()); //*Find what this is used for
+app.use(express.json()); //*Find what this is used for
+
+const URI = process.env.ATLAS_URI;
+
+mongoose.connect(URI, {
+   useNewUrlParser: true,
+   useCreateIndex: true,
+   useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+   //todo Find about this 'once' and 'open' command
+   console.log("mongoDB connection established successfully");
+});
+
+app.get("/", (req, res) => {
+   res.send("<h1>Hello Elliot</h1>");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+   console.log(`App is running @ Port - ${PORT}`);
+});
