c0b035483597d49624eb34a26ba750b40fe64d18
klofaes <vs201100@gmail.com>
2021-01-01 14:50:03 +0530

    Change promises to async await in routes

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 
 let Article = require("../models/article.model");
 
-router.route("/").get((req, res) => {
-   Article.find()
-      .limit(5)
-      .then((articles) => res.json(articles))
-      .catch((err) => res.status(400).json(`Error ${err}`));
+router.route("/").get(async (req, res) => {
+	try {
+		const articles = await Article.find().limit(5);
+		res.json(articles);
+	} catch (err) {
+		res.status(400).json(`Error!!! ${err}`);
+	}
 });
 
 module.exports = router;
