214816ddfa89c03644231ce7cf05063cc0591233
klofaes <vs201100@gmail.com>
2021-01-05 14:25:23 +0530

    Add ID and Password filed with state

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -5,6 +5,7 @@ let Article = require("../models/article.model");
 router.route("/").get(async (req, res) => {
 	try {
 		const articles = await Article.find().limit(6); //8
+		console.log(articles);
 		res.json(articles);
 	} catch (err) {
 		res.status(400).json(`Error!!! ${err}`);
--- src/components/Authentication/Signin.js
+++ src/components/Authentication/Signin.js
@@ -1,11 +1,41 @@
-import React, {Component} from "react";
+import React, { Component, Fragment } from "react";
 
 class Signin extends Component {
-   render() {
-      return (
-         <h1>Hello</h1>
-      );
-   }
+	constructor(props) {
+		super(props);
+		this.state = { id: "", password: "" };
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<form>
+					<div className="id-field">
+						<label for="email-id">Enter ID: </label>
+						<input
+							id="email-id"
+							type="email"
+							value={this.state.id}
+							onChange={(inp) => {
+								this.setState({ id: inp.target.value });
+							}}
+						></input>
+					</div>
+					<div className="pass-field">
+						<label for="password">Enter Password</label>
+						<input
+							id="password"
+							type="password"
+							value={this.state.password}
+							onChange={(inp) => {
+								this.setState({ password: inp.target.value });
+							}}
+						></input>
+					</div>
+				</form>
+			</Fragment>
+		);
+	}
 }
 
 export default Signin;
