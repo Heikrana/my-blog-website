03d715471295e9ad616e844d9510083c653d7ab9
Vaibhav Soni <heikrana@pm.me>
2021-07-15 12:16:35 +0530

    Fix Atlas_URI error

--- routes/Users.js
+++ routes/Users.js
@@ -5,49 +5,48 @@ const bcrypt = require("bcrypt");
 let User = require("../models/User.model");
 
 router.route("/add").post(async (req, res) => {
-   try {
-      const userName = req.body.userCredentials.userName;
-      const Email = req.body.userCredentials.id;
-      const Password = req.body.userCredentials.password;
-      const hashedPassword = await bcrypt.hash(Password, 10); //salt rounds default = 10
-      const newUser = new User({userName, Email, Password: hashedPassword});
+	try {
+		const userName = req.body.userCredentials.userName;
+		const Email = req.body.userCredentials.id;
+		const Password = req.body.userCredentials.password;
+		const hashedPassword = await bcrypt.hash(Password, 10); //salt rounds default = 10
+		const newUser = new User({ userName, Email, Password: hashedPassword });
 
-      await newUser.save();
-      res.json("User Added!");
-   } catch (err) {
-      res.status(400).json(`userAdd: ${err}`);
-   }
+		await newUser.save();
+		res.json("User Added!");
+	} catch (err) {
+		res.status(400).json(`userAdd: ${err}`);
+	}
 });
 
 router.route("/get").post(async (req, res) => {
-   try {
-      const response = await User.find({
-         //getting info of user from DB
-         $or: [
-            {userName: {$eq: req.body.userCredentials.user}},
-            {Email: {$eq: req.body.userCredentials.user}},
-         ],
-      });
+	try {
+		const response = await User.find({
+			//getting info of user from DB
+			$or: [
+				{ userName: { $eq: req.body.userCredentials.user } },
+				{ Email: { $eq: req.body.userCredentials.user } },
+			],
+		});
 
-      // console.log("res", response.length);
+		// console.log("res", response.length);
 
-      if (response.length == 0) {
-         res.status(404).json("Incorrect User");
-      } else {
-         console.log(response);
-         const password = req.body.userCredentials.password;
-         const hash = response[0].Password;
-         //compare password hashes
-         if (await bcrypt.compare(password, hash)) {
-            res.json("Correct");
-         } else {
-            res.status(406).json("Incorrect Password");
-         }
-      }
-
-   } catch (err) {
-      res.status(400).json(`usercheck: ${err}`);
-   }
+		if (response.length == 0) {
+			res.status(404).json("Incorrect User");
+		} else {
+			// console.log(response);
+			const password = req.body.userCredentials.password;
+			const hash = response[0].Password;
+			//compare password hashes
+			if (await bcrypt.compare(password, hash)) {
+				res.json("Correct");
+			} else {
+				res.status(406).json("Incorrect Password");
+			}
+		}
+	} catch (err) {
+		res.status(400).json(`usercheck: ${err}`);
+	}
 });
 
 module.exports = router;
